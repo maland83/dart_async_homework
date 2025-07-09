daaeb0a4eb6dd2be7d1a04ecf1b48e7e62f6242b
Maland <maland83@gmail.com>
2025-07-09 09:03:38 +0300

    feat: add fetchAge and pluralizeYear functions

--- lib/homeworks/lesson_11/hw_futures.dart
+++ lib/homeworks/lesson_11/hw_futures.dart
@@ -1,8 +1,31 @@
 void main(List<String> args) async {
   print('Мене звати ${await fetchName()}');
+
+  final yearsStr = await fetchAge();
+  final years = int.tryParse(yearsStr) ?? 0;
+
+  print('Мені ${pluralizeYear(years)}');
 }
 
 Future<String> fetchName() async {
   await Future.delayed(const Duration(seconds: 2));
   return 'Андрій';
+}
+
+Future<String> fetchAge() async {
+  await Future.delayed(const Duration(milliseconds: 1500));
+  return '25';
+}
+
+String pluralizeYear(int count) {
+  int mod10 = count % 10;
+  int mod100 = count % 100;
+
+  if (mod10 == 1 && mod100 != 11) {
+    return '$count рік';
+  } else if (mod10 >= 2 && mod10 <= 4 && (mod100 < 10 || mod100 >= 20)) {
+    return '$count роки';
+  } else {
+    return '$count років';
+  }
 }
