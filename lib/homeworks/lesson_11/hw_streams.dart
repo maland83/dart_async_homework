19e07b61681dd7a7ac7eba3456aaef086534e5f7
Maland <maland83@gmail.com>
2025-07-10 10:29:17 +0300

    feat[hw-11]: Add Task6

--- lib/homeworks/lesson_11/hw_streams.dart
+++ lib/homeworks/lesson_11/hw_streams.dart
@@ -1,1 +1,18 @@
-void main(List<String> args) {}
+void main(List<String> args) async {
+  Task6();
+}
+
+Future<void> Task6() async {
+  final List<int> intList =
+      List.generate(20, (int index) => index, growable: false).toList();
+
+  final Stream<int> intStream = Stream.fromIterable(intList);
+
+  await for (final val in intStream) {
+    print(val);
+  }
+  print('-----------------------');
+  intStream.listen((event) {
+    print(event);
+  });
+}
