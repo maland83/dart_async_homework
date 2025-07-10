849d45a647b90fce6e7d08f9085a50040d533fc4
Maland <maland83@gmail.com>
2025-07-10 11:43:01 +0300

    feat[HW-11]: Add Task8

--- lib/homeworks/lesson_11/hw_streams.dart
+++ lib/homeworks/lesson_11/hw_streams.dart
@@ -1,18 +1,46 @@
-void main(List<String> args) async {
-  Task6();
+import 'dart:async';
+
+Future<void> main(List<String> args) async {
+  await Task6();
+  await Task7();
+  await Task8();
 }
 
-Future<void> Task6() async {
+Task6() async {
   final List<int> intList =
       List.generate(20, (int index) => index, growable: false).toList();
 
   final Stream<int> intStream = Stream.fromIterable(intList);
-
+  print('--------await for---------------');
   await for (final val in intStream) {
     print(val);
   }
-  print('-----------------------');
+  print('--------listen---------------');
   intStream.listen((event) {
     print(event);
   });
+}
+
+Task7() {
+  final Stream<int> intStrem = Stream<int>.periodic(
+    const Duration(seconds: 1),
+    (count) => count++,
+  ).take(10);
+
+  intStrem.listen((event) {
+    print('$event...');
+  });
+}
+
+Task8() {
+  final controller = StreamController<String>();
+
+  controller.stream.listen(
+    (event) => print(event),
+    onDone: () => print('Стрім завершено'),
+  );
+
+  controller.sink.add('Hello');
+
+  controller.close();
 }
